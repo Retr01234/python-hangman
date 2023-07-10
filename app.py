234871c96274b0a696faffbd6569bf208fe36925
Pave <pitcheroty@gmail.com>
2023-07-10 14:18:01 +0200

    feature: create guessing ability

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 
 def hangman():
   # List of words for the game - I chose Fruits (but can be anything)
-  words = [
+  game_words = [
     "apple",
     "banana",
     "cherry",
@@ -14,10 +14,29 @@ def hangman():
   ]
   
   # Selects a random word from the Words Array
-  word = random.choice(words).lower()
+  word = random.choice(game_words).lower()
 
   # Creating a List to store the Users guessed Letters
-  guessed_letters = []
+  letters_tried = []
 
   # User has maximum 6 Tries to guess the Word (can be changed)
-  tries = 6
+  attempts = 6
+
+  while True:
+    # Display the hangman figure
+    display_hangman(attempts)
+
+    # Display the Game Word with Correct & Incorrect Letters
+    display_word(word, letters_tried)
+
+    # Checks to see if the Word has been completely guessed
+    if guessed_word(word, letters_tried):
+        print("Congratulations! You won!")
+        break
+
+    # Checks to see if the User has anymore attempts
+    if attempts == 0:
+      print("Game over!")
+      print("The Word was:", word)
+      print("Better Luck next time.")
+      break
