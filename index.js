3b25fb1477bcf1df9fa5bbe4e85426ed4dcae983
Pave <pitcheroty@gmail.com>
2023-07-10 23:07:59 +0200

    fix: change package features

--- index.js
+++ index.js
@@ -80,5 +80,5 @@ io.on('connection', (socket) => {
     }
 });
 
-console.log('Starting node on port', process.env.PORT);
-server.listen(process.env.PORT);
+console.log('Starting node on port', process.env.PORT || 8000);
+server.listen(process.env.PORT || 8000);
